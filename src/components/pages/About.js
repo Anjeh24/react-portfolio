afe16550f12e11cd52da1df6754afec3296a5ced
anjeh <junez24@yahoo.com>
2021-09-28 03:57:37 -0400

    Adding code in about and contact files

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,14 +3,9 @@ import React from 'react';
 export default function About() {
   return (
     <div>
-      <h1>About Page</h1>
+      <h1>About Me</h1>
       <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
+        Hi, I'm Anjeh. I'm currently learning to become a Full-Stack web developer. Initially, I stem from a business background but programming picked my interest because it is a very broad field and is here to stay. Also, learning this skill can offer flexibilty between careers and help one understand "the magic behind the screen". Isn't that awesome! 😄 Besides, it feels great creating my first REACT APP 😍 !
       </p>
     </div>
   );
