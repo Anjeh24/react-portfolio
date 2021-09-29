3deecfb4f9caeb6e39c9a5ef431bb350ced447a5
anjeh <junez24@yahoo.com>
2021-09-29 16:18:55 -0400

    Adding picture to header

--- src/components/pages/Header/Header.js
+++ src/components/pages/Header/Header.js
@@ -1,9 +1,14 @@
 import React from "react";
 import './Header.css';
+import  logoUrl from './logo1.jpg';
 
 const Header = () => (
     <div className="header">
-      <p>Hi, I'm called Anjeh and I'm learning to become a Full-Stack web developer.</p>
+        <>
+      <p1>Hi, I'm called Anjeh and I'm learning to become a Full-Stack web developer.</p1>
+      <div ><img src={logoUrl}/></div>
+      </>
     </div>
+    
   )
   export default Header;
