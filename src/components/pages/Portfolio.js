b5cede8918dcb9d6e98730d20b9cb1933e9bd6ac
anjeh <junez24@yahoo.com>
2021-09-30 02:13:56 -0400

    Removing cached files

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,18 +1,23 @@
 import React from 'react';
 
-
+import "./portfolio.css"
 
 export default function Portfolio() {
   return (
     <div className="portfolio">
       <>
+      <main>
       <h1>Portfolio</h1>
+      <span>
       <p>
         This is a showcase of some the projects I've accomplished.
       </p>
-
-      <div className="topleft"><img src={window.location.origin + '/logo2.png'} /></div>
-
+      </span>
+      <br/>
+      <span>
+      <div >  <img src={process.env.PUBLIC_URL + "/logo2.png"} /></div>
+      </span>
+      </main>
       </>
     </div>
   );
