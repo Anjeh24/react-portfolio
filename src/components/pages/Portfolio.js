afe52635de5abd9db535a0271a31dbbd19756554
anjeh <junez24@yahoo.com>
2021-09-30 01:08:13 -0400

    trying to get images to show in portfolio page

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,12 +1,19 @@
 import React from 'react';
 
+
+
 export default function Portfolio() {
   return (
-    <div>
+    <div className="portfolio">
+      <>
       <h1>Portfolio</h1>
       <p>
         This is a showcase of some the projects I've accomplished.
       </p>
+
+      <div className="topleft"><img src={window.location.origin + '/logo2.png'} /></div>
+
+      </>
     </div>
   );
 }
