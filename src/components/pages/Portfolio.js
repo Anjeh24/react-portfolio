40e68755bf373677912efc84104375926e9f4b03
anjeh <junez24@yahoo.com>
2021-09-30 08:43:41 -0400

    updating portfolio page with links to projects

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
-import "./portfolio.css"
+import './portfolio.css';
+
 
 export default function Portfolio() {
   return (
@@ -15,8 +16,26 @@ export default function Portfolio() {
       </span>
       <br/>
       <span>
-      <div >  <img src={process.env.PUBLIC_URL + "/logo2.png"} /></div>
+      <dt><div >  <a href="https://anjeh24.github.io/QUIZ-APP/"><img src={process.env.PUBLIC_URL + "/logo2.png"} /></a></div>
+      <p>Quiz App</p>
+      </dt>
       </span>
+      <br/>
+
+      <span>
+      <dt><div >  <a href="https://anjeh24.github.io/Professional-Portfolio/"><img src={process.env.PUBLIC_URL + "/logo3.jpg"} /></a></div>
+      <p>Professional Portfolio</p>
+      </dt>
+      </span>
+      <br/>
+
+      <span>
+      <dt>
+        <div className="center">  <a href="https://anjeh24.github.io/Work-Day-Scheduler/"><img src={process.env.PUBLIC_URL + "/logo4.jpg"} /></a></div>
+      <p>Work Day Scheduler</p>
+      </dt>
+      </span>
+
       </main>
       </>
     </div>
